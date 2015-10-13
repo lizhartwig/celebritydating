ecc6d518d38e70365cf9f520719318d861a8f023
Elizabeth Hartwig & Sara Stillwell <student@epicodus.com>
2015-10-12 20:49:16 -0700

    make two variable work

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   $("form#calculatedate").submit(function(event) {
     var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
 
-    if (age <= 25) {
+    if (age <= 25 && gender === "male") {
     var celebrity = ("Selena Gomez");
     $("#celebrity").text(celebrity);
       $("#answer").show();
+    } else {
+      $("#celebrity").text("Jennifer Aniston");
+        $("#answer").show();
     }
+
 
     event.preventDefault();
   });
